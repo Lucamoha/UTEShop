f708d4e5df14cdb4bfb64592fe6b383e805312fc
nvk3005 <nguyenvankent3005@gmail.com>
2025-09-16 16:54:14 +0700

    Thêm file constant chứa đường dẫn thư mục thực tế trên máy ( dùng cho việc đoc và upload ảnh)

---
+++ src/main/java/com/uteshop/util/Constant.java
@@ -0,0 +1,9 @@
+package com.uteshop.util;
+
+public class Constant {
+        public static final String DIR = System.getProperty("user.dir");
+
+        public static void main(String[] args) {
+            System.out.println("Current directory: " + DIR);
+        }
+}
