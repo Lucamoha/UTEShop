1b89c784a22594bb77eb867798446ed499f1a633
nvk3005 <nguyenvankent3005@gmail.com>
2025-10-05 23:47:15 +0700

    home page

--- src/main/java/com/uteshop/util/Constant.java
+++ src/main/java/com/uteshop/util/Constant.java
@@ -1,9 +1,15 @@
 package com.uteshop.util;
 
+import java.io.File;
+
 public class Constant {
-        public static final String DIR = System.getProperty("user.dir");
-        
-        public static final String ADMIN = "admin";
+    public static final String DIR = System.getProperty("user.dir")
+            + File.separator + "src"
+            + File.separator + "main"
+            + File.separator + "webapp"
+            + File.separator + "uploads";
+
+    public static final String ADMIN = "admin";
         public static final String EMPLOYEE = "employee";
         public static final String USER_ = "user";
 
